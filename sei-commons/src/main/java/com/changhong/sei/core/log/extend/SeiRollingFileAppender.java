8de6397bf9005a23aa7dc870f58314ec86398e34
Vision.Mac <chao2.ma@changhong.com>
2020-03-25 16:44:23 +0800

    修改日志bug

--- sei-commons/src/main/java/com/changhong/sei/core/log/extend/SeiRollingFileAppender.java
+++ sei-commons/src/main/java/com/changhong/sei/core/log/extend/SeiRollingFileAppender.java
@@ -32,9 +32,22 @@ public class SeiRollingFileAppender extends RollingFileAppender<ILoggingEvent> {
         markerFilter.start();
         super.addFilter(markerFilter);
 
+        /*
+            如果返回值是neutral，就会有下一个filter进行判断
+            如果已经没有后续filter，那么会对这个日志事件进行处理
+            如果判断是accept，那么就会立即对该日志事件进行处理，不再进行后续判断
+         */
         LevelFilter levelFilter = new LevelFilter();
-        //非ERROR级别的日志，被过滤掉
+        //非WARN级别的日志，被过滤掉
         levelFilter.setLevel(Level.WARN);
+        levelFilter.setOnMatch(FilterReply.ACCEPT);
+        levelFilter.setOnMismatch(FilterReply.NEUTRAL);
+        levelFilter.start();
+        super.addFilter(levelFilter);
+
+        levelFilter = new LevelFilter();
+        //非ERROR级别的日志，被过滤掉
+        levelFilter.setLevel(Level.ERROR);
         levelFilter.setOnMatch(FilterReply.ACCEPT);
         levelFilter.setOnMismatch(FilterReply.DENY);
         levelFilter.start();
